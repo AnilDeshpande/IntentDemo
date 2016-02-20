b79f2c7f669e98cc1ed1405660529922c457634d
AnilDeshpande <anildesh82@gmail.com>
2016-02-20 08:07:25 +0530

    First Commit
    This is the first commit of the program

---
+++ app/src/main/java/intentdemo/sample/com/intentdemo/ActivitySecond.java
@@ -0,0 +1,16 @@
+package intentdemo.sample.com.intentdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by anildeshpande on 18/02/16.
+ */
+public class ActivitySecond extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layoyt_activity_second);
+    }
+}
---
+++ app/src/main/java/intentdemo/sample/com/intentdemo/MainActivity.java
@@ -0,0 +1,53 @@
+package intentdemo.sample.com.intentdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button buttonNextActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonNextActivity=(Button)findViewById(R.id.buttonNextActivity);
+        buttonNextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this,"Button Clicked",Toast.LENGTH_SHORT).show();
+                Intent secondActIntent=new Intent(MainActivity.this,ActivitySecond.class);
+                startActivity(secondActIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
